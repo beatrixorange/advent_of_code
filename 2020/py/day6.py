8d54107115d1689306d615b8768b116e9db518f3
beatrixorange <detmer56@gmail.com>
2021-01-16 17:08:31 +0100

    part two day6

--- 2020/py/day6.py
+++ 2020/py/day6.py
@@ -2,12 +2,33 @@ f = open("/home/detmer/programming/aoc/2020/aoc_input/input_day6.txt", "r")
 
 all_forms = f.read()
 
-total = 0
+total1 = 0
+total2 = 0
 
 forms = all_forms.split("\n\n")
 for form in forms:
-	u_form= form.replace("\n",'')
+	#part two
+	all_freq = {}
+	m = form.split("\n")
+	if m[len(m)-1] == '':
+		m.pop(len(m)-1)
+	for c in form:
+		if c in all_freq:
+			all_freq[c] += 1
+		else:
+			all_freq[c] = 1
+		all_freq['\n'] = 0
+	for f in all_freq.items():
+		print(f)
+		if f[1] == len(m):
+			print(f[1])
+			total2 += 1
+	
+	#part one
+	u_form = form.replace("\n",'')
 	s = set(u_form)
-	total = a + len(s)
-	print()
-print(total)
+	total1 = total1 + len(s)
+
+
+print(total1)
+print(total2)
